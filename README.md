050df6c88d3c870257ab3e28b3cfadadac513ef2
jackie <jackie198512@126.com>
2015-02-09 16:24:12 +0800

    走进鹏华

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 `.c-abc{}` 
 `.l-abc{}`
 
-2.除 `.abc a{}` 以外，不允许使用标签做css命名 比如 `.abc div{}` `.abc li{}`
+2.除 `.abc a{}` 以外，不允许使用标签做css命名, 如 `.abc div{}` `.abc li{}`
 
-3. `.active{}` `.selected{}` 可以使用，但必须有父class `.c-abc .active{}` 
+3. `.active{}` `.selected{}` 可以使用，但必须有父class,如 `.c-abc .active{}` 
--- js/phfund.js
+++ js/phfund.js
@@ -5,6 +5,29 @@
 (function (window, document, undefined) {
 
 
+    /*
+    * 登陆框切换*/
+    $('.h_switch_btn').bind('click', function () {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $($(this).siblings('.h_login_inner')[0]).show();
+            $($(this).siblings('.h_login_inner')[1]).hide();
+        } else {
+            $(this).addClass('active');
+            $($(this).siblings('.h_login_inner')[0]).hide();
+            $($(this).siblings('.h_login_inner')[1]).show();
+
+        }
+    })
+
+    /*
+    * 关于 左侧菜单
+    * */
+
+    $('.c_page_menu_ul li').click(function () {
+        $(this).toggleClass('active');
+    });
+
 
 
 })(window, window.document);
