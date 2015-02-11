3d587d111880b211800655a633dfa167fe5c37e2
mingleliu <568209027@qq.com>
2015-02-11 22:32:12 +0800

    右边banner的添加

--- js/phfund.js
+++ js/phfund.js
@@ -29,5 +29,27 @@
     });
 
 
+    /**
+     * 右侧的banner
+     * **/
+    $(window).scroll(function(){
+        var _top = $(window).scrollTop(),
+            _width = $(window).width(),
+            _right = 0,
+            _banner = $('.l_right_banner'),
+            _time = 200;
+        if(_width > 1200){
+            _right = (_width - 1200) /2 - 44;
+        }
+        _banner.css('right',_right + 'px');
+        console.log(_top < 500)
+        if(_top < 590){
+            _banner.stop().animate({'top':(590 - _top) + 'px'},_time);
+        }else{
+            _banner.stop().animate({'top': '10px'},_time);
+        }
+
+    });
+
 
 })(window, window.document);
