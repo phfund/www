8afceeaffc39648decf044d007148cc68e2091a9
jackie <jackie198512@126.com>
2015-04-10 11:38:21 +0800

    1首页: fix ie6

--- js/unitpngfix.js
+++ js/unitpngfix.js
@@ -21,8 +21,8 @@ pngfix = function () {
             if (el.src && el.src.match(ip) && !es.filter) {
                 es.height = el.height;
                 es.width = el.width;
-                es.filter = al + el.src + "',sizingMethod='crop')";
-                el.src = clear;
+                //es.filter = al + el.src + "',sizingMethod='crop')";
+                //el.src = clear;
             } else {
                 if (elb.match(ip)) {
                     var path = elb.split('"'), rep = (elc.backgroundRepeat == 'no-repeat') ? 'crop' : 'scale', elkids = el.getElementsByTagName('*'), j = elkids.length;
